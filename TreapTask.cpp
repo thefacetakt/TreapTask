7eef4cf0011712f275c9204024d63c489aa05f2d
Stepan Kargaltsev <stepan.kargaltsev@phystech.edu>
2015-02-13 21:59:44 +0300

    Rough copy of realisation completed, but was not tested or even compiled

--- TreapTask.cpp
+++ TreapTask.cpp
@@ -147,6 +147,30 @@ private:
         return std::pair<Node *, Node *>(node, temp.second);
     }
 
+
+    //call only if node-referenced array is decreasingly sorted 
+    std::pair<Node *, Node *> splitByElement(Node *node, int element)
+    {
+         if (!node)
+        {
+            return std::pair<Node *, Node *>(nullptr, nullptr);
+        }
+
+        push(node);
+
+        if (node->value <= element)
+        {
+            std::pair<Node *, Node *> temp = split(node->left, i);
+            node->left = temp.second;
+            recalc(node);
+            return std::pair<Node *, Node *>(temp.first, node);
+        }
+        std::pair<Node *, Node *> temp = split(node->right, i - sizeOf(node->left) - 1U);
+        node->right = temp.first;
+        recalc(node);
+        return std::pair<Node *, Node *>(node, temp.second);
+    }
+
     Node * merge(Node *left, Node *right)
     {
         if (!left || !right)
@@ -200,5 +224,38 @@ public:
         root = merge(T1.first, merge(T2.first, T2.second));
     }
 
-};
+    bool next_permutation(size_t i, size_t j)
+    {
+        /*
+            |                                             root                                                                   |
+            |T1.first|                                   T1.second (T2)                                                          |
+            |        |                                  segment (parts)                                                |T2.second|
+            |        |                  prefixParts         |             decreasingPart(suffixParts)                  |         |
+            |        |                 | elementToSwap      |             middleParts           |                      |         |     
+            |T1.first|prefixParts.first| elementToSwap      |middleParts.first|elementToSwapWith|  suffixParts.second  |T2.second| - result of split
 
+       */
+        auto T1 = split(root, i);
+        auto T2 = split(T1.second, j - i + 1);
+        Node *segment = T2.first;
+        if (segment->longestNonIncreasingSuffix == segment->size)
+        {
+            reverse(segment);
+            root = merge(T1.first, merge(segment, T2.second));
+            return false;
+        }
+        auto parts = split(segment, segment->size - segment->longestNonIncreasingSuffix);
+        Node *decreasingPart = parts.second;
+        auto prefixParts = split(parts.first, parts.first->size - 1U);
+        Node *elementToSwap = prefixParts.second;
+        auto suffixParts = splitByElement(decreasingPart, elementToSwap->value);
+        auto middleParts = split(suffixParts.first, suffixParts.first->size - 1U);
+        Node *elementToSwapWith = middleParts.second;
+        Node *newSuffix = merge(middleParts.first, merge(elementToSwap, suffixParts.second));
+        reverse(newSuffix);
+
+        segment = merge(prefixParts.first, merge(elementToSwapWith, newSuffix));
+        root = merge(T1.first, merge(segment, T2.second));
+        return true;
+    }
+};
