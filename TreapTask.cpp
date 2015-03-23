531d794768d49bb208c7945af4cf2b55c4af9e73
Stepan Kargaltsev <stepan.kargaltsev@phystech.edu>
2015-03-23 10:48:46 +0300

    optinal compilation added

--- TreapTask.cpp
+++ TreapTask.cpp
@@ -11,6 +11,7 @@ int main(int argc, char **argv)
     Tester Test(atoi(argv[1]), atoi(argv[2]), atoi(argv[3]));
     Test.srand(atoi(argv[4]));
     std::vector <long long> treapResult = Test(new NTreap::Treap());
+#ifdef _COMPARE_
     Test.srand(atoi(argv[4]));
     std::vector <long long> stupidResult = Test(new Stupid());
     if (treapResult != stupidResult)
@@ -25,4 +26,5 @@ int main(int argc, char **argv)
     {
         printf("OK TEST %d\n", atoi(argv[4]));
     }
+#endif
 }
